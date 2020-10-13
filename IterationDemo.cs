87f283b03a582b0c64ca95151d68cc172e7c4ac0
sushantthapa <sushant.thapa55@gmail.com>
2020-10-13 13:15:01 +0545

    Commit for While Loop

--- IterationDemo.cs
+++ IterationDemo.cs
@@ -1,22 +1,24 @@
 using System;
 class IterationDemo
 {
-    static void Main(string[] args)
+    static void Main21(string[] args)
     {
-        IterationDemo obji=new IterationDemo();
+        IterationDemo obji = new IterationDemo();
         obji.LearnForLoop();
+        obji.LearnWhileLoop();
 
     }
     void LearnForLoop()
+
     {
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine("Nepal is a beautiful country.");
         }
-        int[] numbers = {12,23,1232,43,123};
-        foreach(int x in numbers)
+        int[] numbers = { 12, 23, 1232, 43, 123 };
+        foreach (int x in numbers)
         {
-            if(x%2==0)
+            if (x % 2 == 0)
             {
                 Console.WriteLine($"{x} is even.");//USE OF STRING INTERPOLATION
             }
@@ -29,4 +31,25 @@ class IterationDemo
 
 
     }
+
+    void LearnWhileLoop()
+    {
+        char opinions = 'Y';
+        while (opinions == 'Y')
+        {
+
+            Console.WriteLine("Nepal is landlocked country");
+            Console.WriteLine("Print One more time?");
+            opinions = Console.ReadKey().KeyChar;
+
+        }
+
+        do
+        {
+            Console.WriteLine("There are 77 district in Nepal");
+            Console.WriteLine("Print One more Time?");
+            opinions = Console.ReadKey().KeyChar;
+        } while (opinions == 'Y');
+    }
+
 }
